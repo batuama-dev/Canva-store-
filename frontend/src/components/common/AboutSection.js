4b3e70a843a54ec09ba8cdc75cd599b2da17caf7
Israël batuama <ibatuama@gmail.com>
2026-02-08 20:26:21 +0100

    feat: Augmenter la hauteur de l'image de la section 'À propos' de 20%

--- frontend/src/components/common/AboutSection.js
+++ frontend/src/components/common/AboutSection.js
@@ -9,7 +9,7 @@ const AboutSection = () => {
           <div className="w-full lg:w-1/2 px-4 mb-8 lg:mb-0">
             <img 
               src="/Apropos_temply.jpeg" 
-              className="w-full h-auto max-h-96 rounded-2xl shadow-lg" 
+              className="w-full h-auto max-h-[28rem] rounded-2xl shadow-lg" 
               alt="About Us" 
             />
           </div>
@@ -22,17 +22,11 @@ const AboutSection = () => {
                 Vous passez trop de temps à chercher l’inspiration avant de commencer un design ?
                 Vous avez déjà perdu un client ou livré en retard faute de visuels prêts à temps ?
                 Vous aimeriez créer des designs professionnels sans repartir de zéro à chaque projet ?
-              </p>
-              <p className="text-gray-600 mb-4 text-justify">
                 Templyfast est né pour répondre à ces problèmes.
               </p>
               <p className="text-gray-600 mb-4 text-justify">
-                Nous sommes une plateforme congolaise dédiée à la vente de templates Canva professionnels, pensés pour les réalités, les activités et le style du marché africain. Que vous soyez graphiste, entrepreneur, community manager ou débutant motivé, Templyfast vous permet de produire plus vite, mieux et de manière rentable.
-              </p>
-              <p className="text-gray-600 text-justify">
-                Nous misons sur la qualité, l’innovation et surtout la simplicité : paiement local adapté à la RDC, livraison quasi instantanée par email, et designs alignés avec les tendances actuelles.
-              </p>
-              <p className="text-gray-600 text-justify">
+                Nous sommes une plateforme congolaise dédiée à la vente de templates Canva professionnels, pensés pour les réalités, les activités et le style du marché africain. Que vous soyez graphiste, entrepreneur, community manager ou débutant motivé, Templyfast vous permet de produire plus vite, mieux et de manière rentable.           
+                Nous misons sur la qualité, l’innovation et surtout la simplicité : paiement local adapté à la RDC, livraison quasi instantanée par email, et designs alignés avec les tendances actuelles.             
                 Templyfast, ce n’est pas seulement des templates.
                 C’est une solution pour ceux qui refusent de perdre du temps, de l’argent et des opportunités à cause du design.
               </p>
